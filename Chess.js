f73c2d2752e8bdd0078b67a36bdbdb2e5e149f90
SimeonKirilov031 <simeonkirilov031@gmail.com>
2019-08-27 13:43:28 +0300

    Update Chess.js

--- Chess.js
+++ Chess.js
@@ -61,7 +61,7 @@ class queen {
     };
 };
 
-let posIncrement = 'A';
+let posIncrement = 'a';
 
 
 function createFigures(){
@@ -69,23 +69,24 @@ for (var i = 1; i < 9; ++i) {
     this["wp"+i] = new pawn(posIncrement, 2, 'white', false);
     this["bp"+i] = new pawn(posIncrement, 2, 'black', false);
     posIncrement = nextChar(posIncrement);
-wr1 = new rook('A', 1, 'white', false);
-wr2 = new rook('H', 1, 'white', false);
-br1 = new rook('A', 8, 'black', false);
-br2 = new rook('H', 8, 'black', false);
-wk1 = new knight('B', 1, 'white', false);
-wk2 = new knight('G', 1, 'white', false);
-bk1 = new knight('B', 8, 'black', false);
-bk2 = new knight('H', 8, 'black', false);
-wb1 = new bishop('C', 1, 'white', false);
-wb2 = new bishop('F', 1, 'white', false);
-bb1 = new bishop('C', 8, 'black', false);
-bb2 = new bishop('F', 8, 'black', false);
-wk = new king('E', 1, 'white', false);
-bk = new king('E', 8, 'black', false);
-wq = new queen('D', 1, 'white', false);
-bq = new queen('D', 8, 'black', false);
-};
+  };
+
+wr1 = new rook('a', 1, 'white', false);
+wr2 = new rook('h', 1, 'white', false);
+br1 = new rook('a', 8, 'black', false);
+br2 = new rook('h', 8, 'black', false);
+wk1 = new knight('b', 1, 'white', false);
+wk2 = new knight('g', 1, 'white', false);
+bk1 = new knight('b', 8, 'black', false);
+bk2 = new knight('h', 8, 'black', false);
+wb1 = new bishop('c', 1, 'white', false);
+wb2 = new bishop('f', 1, 'white', false);
+bb1 = new bishop('c', 8, 'black', false);
+bb2 = new bishop('f', 8, 'black', false);
+wk = new king('e', 1, 'white', false);
+bk = new king('e', 8, 'black', false);
+wq = new queen('d', 1, 'white', false);
+bq = new queen('d', 8, 'black', false);
 };
 
 
@@ -99,3 +100,76 @@ function showPawns(){
 
     };
 
+// Compare array coordinates and fill values
+
+    let arei = ['a2', 'b2', 'c2', 'd2'];
+
+    for (var j=0; j<arei.length; j++) {
+    for (var i=1; i<9; i++) {
+    if(this["wp"+i].posX + this["wp"+i].posY == arei[j]){
+      arei[j] = this["wp"+i];
+     };
+    };
+
+    };
+    console.log(arei);
+
+  //  if (this.posX != new.posX && this.posY != new.posY){     Rook wrong move
+  //  if (this.posX - new.posX != this.posY - new.posY)   Bishop wrong move          use abs()
+  //  if (this.posX + new.posX == this.posX + 3 & this.posY + new.posY == this.posY + 1 || this.posX + new.posX == this.posX -3 & this.posY + new.posY == -1) 
+    let test
+  test = wp1.posX.charCodeAt(0) - 96;
+  console.log(wp1.posX);
+  console.log(test);
+
+
+    let nue = {
+      posX:'a',
+      posY: 1
+    };
+
+
+   /* 
+   
+    // Rook, Bishop, and Queen pieces validation for later integration
+
+   function move (oldPosX, oldPosY, newPosX, newPosY){
+      arei.forEach(asd);
+
+      };
+        function asd(){
+      if (oldPosX + oldPosY == arei[i]){
+        let figureType = arei[i].name;
+        if (figureType == 'rook'){
+          if(oldPosX != newPosX && oldPosY != newPosY){
+            console.log('WRONG');
+          }
+            else{
+              arei[i].posX = newPosX;
+              arie[i].posY = newPosY;
+            };
+          }
+          else if(figureType == 'bishop'){
+            if(abs(oldPosX.charCodeAt(0)-96 - newPosX.charCodeAt(0) - 96) != abs(oldPosY - newPosY)){
+              console.log('WRONG');
+            }
+            else{
+              arei[i].posX = newPosX;
+              arie[i].posY = newPosY;
+            }
+          }
+          else if(figureType == 'queen'){
+            if(oldPosX != newPosX && oldPosY != newPosY || abs(oldPosX.charCodeAt(0)-96 - newPosX) != abs(oldPosY - newPosY)){
+              console.log('WRONG');
+            }
+              else{
+                arei[i].posX = newPosX;
+                arie[old].posY = newPosY;
+              };
+            };
+          }
+          else if(figureType == 'knight'){
+
+          }
+          };
+      };*/
